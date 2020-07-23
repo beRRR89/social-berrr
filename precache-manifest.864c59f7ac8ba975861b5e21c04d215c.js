6808b92b25edce7cd07fb7ab8728bb86b315e13e
Evgeny Chencov <epic.berrr@gmail.com>
2020-07-23 15:08:51 +0300

    Updates

---
+++ precache-manifest.864c59f7ac8ba975861b5e21c04d215c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5889327fd163fe51af8442f85e8c8e36",
+    "url": "/social-berrr/index.html"
+  },
+  {
+    "revision": "91ed1f5f2613c011c21f",
+    "url": "/social-berrr/static/css/4.ab5e798b.chunk.css"
+  },
+  {
+    "revision": "16280efd2a1893409833",
+    "url": "/social-berrr/static/css/5.32e39245.chunk.css"
+  },
+  {
+    "revision": "840fa751ce2ff7c5ffc5",
+    "url": "/social-berrr/static/css/6.b5b32d45.chunk.css"
+  },
+  {
+    "revision": "93346a22d8f120815c6d",
+    "url": "/social-berrr/static/css/7.6674f145.chunk.css"
+  },
+  {
+    "revision": "ca4e9fbfe5439ebe7847",
+    "url": "/social-berrr/static/css/main.72d1300f.chunk.css"
+  },
+  {
+    "revision": "859aa26b6acc4603afc8",
+    "url": "/social-berrr/static/js/0.431fde74.chunk.js"
+  },
+  {
+    "revision": "243e9eea98e909f31a90",
+    "url": "/social-berrr/static/js/3.cabcaa8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-berrr/static/js/3.cabcaa8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ed1f5f2613c011c21f",
+    "url": "/social-berrr/static/js/4.bf14bdfc.chunk.js"
+  },
+  {
+    "revision": "16280efd2a1893409833",
+    "url": "/social-berrr/static/js/5.4940dfe5.chunk.js"
+  },
+  {
+    "revision": "840fa751ce2ff7c5ffc5",
+    "url": "/social-berrr/static/js/6.24e283ee.chunk.js"
+  },
+  {
+    "revision": "93346a22d8f120815c6d",
+    "url": "/social-berrr/static/js/7.03102620.chunk.js"
+  },
+  {
+    "revision": "ca4e9fbfe5439ebe7847",
+    "url": "/social-berrr/static/js/main.637d30a8.chunk.js"
+  },
+  {
+    "revision": "53efd4b27838d1a98519",
+    "url": "/social-berrr/static/js/runtime-main.33a12d03.js"
+  },
+  {
+    "revision": "57c49ee888bd98759c281c2eff692562",
+    "url": "/social-berrr/static/media/KannaWhat.57c49ee8.png"
+  },
+  {
+    "revision": "d836564fb375cc08e741282e7bd2b9ee",
+    "url": "/social-berrr/static/media/user.d836564f.png"
+  }
+]);
